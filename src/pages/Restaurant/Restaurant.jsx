988aefb43a4a497621b500432ac170a05ff05ce2
kralin.front <kralin.front@gmail.com>
2023-02-11 18:08:13 +0300

    add restaraunt page, buttons

--- src/pages/Restaurant/Restaurant.jsx
+++ src/pages/Restaurant/Restaurant.jsx
@@ -1,10 +1,37 @@
 import { RestaurantHeader } from "./ui/RestaurantHeader"
-
+import primeLogo from 'assets/backgraund/primeLogo.png'
+import styles from './styles.module.scss'
 
 function Restaurant() {
 
-    return <div>
+    return <div className={styles.wrapper}>
         <RestaurantHeader />
+        <div className={styles.about}>
+            <div className={styles.aboutItem} >СТЕЙКИ НА УГЛЯХ</div>
+            <div className={styles.aboutItem} >КРАФТОВОЕ ПИВО</div>
+            <div className={styles.aboutItem} >СПОРТИВНЫЕ ТРАНСЛЯЦИИ</div>
+            <div className={styles.aboutItem} >КОНДИТЕРСКАЯ</div>
+            <div className={styles.aboutItem} >ЖИВАЯ МУЗЫКА</div>
+            <div className={styles.aboutItem} >ДАРТС</div>
+            <div className={styles.aboutItem} >КИКЕР</div>
+            <div className={styles.aboutItem} >АКЦИИ</div>
+        </div>
+        <div className={styles.aboutRestaurants}>
+            <div className={styles.left}>
+                <div><button className={`${styles.leftBtn}`}>
+                    <h3 className={styles.title}>КОЛЕСНИКОВА, 38</h3>
+                </button>
+                </div>
+                <div><button className={styles.orderBtn}>ЗАБРОНИРОВАТЬ СТОЛ</button></div>
+            </div>
+            <div><img src={primeLogo} className={styles.logo} alt='logo' /></div>
+            <div className={styles.right}>
+                <div><button className={`${styles.rightBtn} ${styles.title}}`}>
+                    <h3 className={styles.title}>КУЛЬМАН,4</h3>
+                </button></div>
+                <div><button className={styles.orderBtn}>ЗАБРОНИРОВАТЬ СТОЛ</button></div>
+            </div>
+        </div>
     </div>
 }
 
--- src/pages/Restaurant/ui/RestaurantHeader/RestaurantHeader.jsx
+++ src/pages/Restaurant/ui/RestaurantHeader/RestaurantHeader.jsx
@@ -1,19 +1,13 @@
 import styles from './styles.module.scss'
-import primeLogo from 'assets/backgraund/primeLogo.png'
-import { MenuHeader } from 'pages/Menu/ui/MenuHeader'
+import coal from 'assets/backgraund/coal.jpg'
 
 function RestaurantHeader() {
-
     return (
         <div className={styles.wrapper}>
-            <div className={styles.header}>
-                <img src={primeLogo} className={styles.logo} alt='logo'/>
-            </div>
             <div className={styles.headerImg}>
-                <img src={MenuHeader} className={styles.img} alt='foto' />
+                <img src={coal} className={styles.img} alt='foto' />
             </div>
-
-
+            <div className={styles.title}>Жарим с любовью...</div>
         </div>
     )
 }
